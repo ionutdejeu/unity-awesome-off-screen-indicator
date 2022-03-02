fb952c7b2161801c944a6e920d2601ff8cce5bbd
ionut.dejeu <ionut.dejeu@gmail.com>
2022-03-02 17:14:52 +0200

    * OffScreenIndicatorSystem.cs:
    * OffScreenIndicatorMath.cs: WIP for core math and debug layer

--- Assets/Scripts/OffScreenIndicatorMath.cs
+++ Assets/Scripts/OffScreenIndicatorMath.cs
@@ -8,8 +8,31 @@ public class OffScreenIndicatorMath
     {
         return main.WorldToScreenPoint(targetPos);
     }
-    public static bool IsTargetVisibleOnScreen(Screen s, Vector3 screenPos)
+
+    public void drawDebugRect(Rect rect)
     {
-        return screenPos.z > 0 && screenPos.x > 0;
+        Debug.DrawLine(new Vector3(rect.x, rect.y), new Vector3(rect.x + rect.width, rect.y), Color.green);
+        Debug.DrawLine(new Vector3(rect.x, rect.y), new Vector3(rect.x, rect.y + rect.height), Color.red);
+        Debug.DrawLine(new Vector3(rect.x + rect.width, rect.y + rect.height), new Vector3(rect.x + rect.width, rect.y), Color.green);
+        Debug.DrawLine(new Vector3(rect.x + rect.width, rect.y + rect.height), new Vector3(rect.x, rect.y + rect.height), Color.red);
     }
+
+    /// <summary>
+    /// Determine if the specified screen position is visible in screen coordintates
+    /// </summary>
+    /// <param name="s"></param>
+    /// <param name="screenPos"></param>
+    /// <returns></returns>
+    public static bool IsTargetVisibleOnScreen(Vector3 screenPos)
+    {
+        return screenPos.z > 0 && screenPos.x > 0 &&  screenPos.x <Screen.width && screenPos.y > 0 && screenPos.y < Screen.height;
+    }
+
+    
+    public float AngleOfRotationTorwardsScreenPos(Vector3 direction)
+    {
+        return Mathf.Atan2(direction.x, direction.y) * Mathf.Deg2Rad - 90;
+    }
+
+    
 }
